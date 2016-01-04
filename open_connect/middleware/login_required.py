4ecb7c7a4e154111784179d9c899d008b98d7835
Nick Catalano <nickcatal@gmail.com>
2016-01-04 01:12:32 -0600

    Update login_required middleware
    Currently AJAX requests were being sent through the login flow, which
    could cause problems if you had multiple windows open and active AJAX
    requests going.
    
    For example, if you had one window open which was checking for new
    messages and you logged out in another window, the next check for new
    messages would attempt to go through the login flow. As the login flow
    keeps track of the URL you're attempting to access when you first start
    the flow it's possible that you could end up being redirected to the
    check-for-new-messages JSON endpoint instead of an actual HTTP page.
    
    This also adds no-cache headers to non-logged-in sessions. The headers
    added should cover almost all cases of client-side caching on all
    browsers.
    
    This also cleans up the tests for the middleware quite a bit. As this
    code predates some of the more modern testing mixins that are included
    with Connect, this was an opportunity to use some of those to make the
    tests slightly less likely to cause problems later as we change Connect.

--- open_connect/middleware/login_required.py
+++ open_connect/middleware/login_required.py
@@ -2,9 +2,11 @@
 # pylint: disable=no-self-use
 
 import re
+import json
 
 from django.conf import settings
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseBadRequest
+from django.views.decorators.cache import patch_cache_control
 
 
 EXEMPT_URLS = [re.compile(settings.LOGIN_URL.lstrip('/'))]
@@ -30,6 +32,30 @@ class LoginRequiredMiddleware(object):
             if path == '':
                 return
             if not any(m.match(path) for m in EXEMPT_URLS):
-                redirect_to = '%s?next=%s' % (settings.LOGIN_URL,
-                                              request.path_info)
-                return HttpResponseRedirect(redirect_to)
+                if request.is_ajax():
+                    # For AJAX requests, return a 400 (which will trigger
+                    # jquery's ajax error functionality) and include the error
+                    # in a way consistent with the rest of Connect. This is to
+                    # prevent AJAX requests from ever being sent through the
+                    # login flow (which can cause problems.)
+                    response = HttpResponseBadRequest(
+                        json.dumps({
+                            'success': False,
+                            'errors': [
+                                'You Must Be Logged In',
+                            ]
+                        }),
+                        content_type='application/json'
+                    )
+                else:
+                    redirect_to = '%s?next=%s' % (settings.LOGIN_URL,
+                                                  request.path_info)
+                    response = HttpResponseRedirect(redirect_to)
+
+                # Break the client-side cache on all possible browsers on all
+                # protocols.
+                patch_cache_control(
+                    response, no_cache=True, no_store=True,
+                    must_revalidate=True, max_age=0, private=True,
+                    proxy_revalidate=True, s_maxage=0)
+                return response
