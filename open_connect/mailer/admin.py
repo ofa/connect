e4b7ccc850e5f25b4cd052c17c4f2de30c30b107
Nick Catalano <nickcatal@gmail.com>
2017-02-19 01:07:23 -0600

    Better allowing browsing of Unsubscribes in admin (#29)
    * Better allowing browsing of Unsubscribes in admin
    
    * Adjust docstring in unsubscribe admin

--- open_connect/mailer/admin.py
+++ open_connect/mailer/admin.py
@@ -4,4 +4,27 @@ from django.contrib import admin
 
 from open_connect.mailer.models import Unsubscribe
 
-admin.site.register(Unsubscribe)
+
+class UnsubscribeAdmin(admin.ModelAdmin):
+    """Admin for Unsubscribes"""
+    list_display = ('address', 'user', 'source')
+    list_filter = ('source',)
+    readonly_fields = [
+        'user'
+    ]
+    search_fields = ['address']
+
+    def get_queryset(self, request):
+        """
+        Get the queryset for the admin
+
+        In order to reduce the number of hits to the database we can tell
+        django to join in the `User` table when running the query that pulls in
+        the Unsubscribes.
+        """
+        queryset = super(UnsubscribeAdmin, self).get_queryset(request)
+        queryset = queryset.select_related('user')
+        return queryset
+
+
+admin.site.register(Unsubscribe, UnsubscribeAdmin)
